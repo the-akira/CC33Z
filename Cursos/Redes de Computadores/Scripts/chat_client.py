cf4b84307fb80eeeb75e4074586b9fbfbb134f10
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-12 18:14:26 -0300

    Chat Client atualizado

--- Cursos/Redes de Computadores/Scripts/chat_client.py
+++ Cursos/Redes de Computadores/Scripts/chat_client.py
@@ -6,7 +6,7 @@ from colorama import Fore, init, Back
 
 init()
 colors = [
-	Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
+    Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
     Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
     Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
     Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
