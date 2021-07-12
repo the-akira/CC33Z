806b24d4808ebef7ae5a8f8b24d0d5a3da34ddaf
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-12 19:12:54 -0300

    Music Player atualizado

--- Cursos/Desenvolvimento Web/Music Player/script.js
+++ Cursos/Desenvolvimento Web/Music Player/script.js
@@ -90,63 +90,63 @@ function setProgress(e) {
 
 //get duration & currentTime for Time of song
 function DurTime(e) {
-	const {duration,currentTime} = e.srcElement;
-	var sec;
-	var sec_d;
+  const {duration,currentTime} = e.srcElement;
+  var sec;
+  var sec_d;
 
-	// define minutes currentTime
-	let min = (currentTime==null)? 0:
-	 Math.floor(currentTime/60);
-	 min = min <10 ? '0'+min:min;
+  // define minutes currentTime
+  let min = (currentTime==null)? 0:
+   Math.floor(currentTime/60);
+   min = min <10 ? '0'+min:min;
 
-	// define seconds currentTime
-	function get_sec(x) {
-		if(Math.floor(x) >= 60){
+  // define seconds currentTime
+  function get_sec(x) {
+    if(Math.floor(x) >= 60){
 			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec = Math.floor(x) - (60*i);
-					sec = sec <10 ? '0'+sec:sec;
-				}
-			}
-		} else {
-		 	sec = Math.floor(x);
-		 	sec = sec <10 ? '0'+sec:sec;
-		 }
-	} 
+      for (var i = 1; i<=60; i++){
+        if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+          sec = Math.floor(x) - (60*i);
+          sec = sec <10 ? '0'+sec:sec;
+        }
+      }
+    } else {
+      sec = Math.floor(x);
+      sec = sec <10 ? '0'+sec:sec;
+    }
+  } 
 
-	get_sec(currentTime,sec);
+  get_sec(currentTime,sec);
 
-	// change currentTime DOM
-	currTime.innerHTML = min +':'+ sec;
+  // change currentTime DOM
+  currTime.innerHTML = min +':'+ sec;
 
-	// define minutes duration
-	let min_d = (isNaN(duration) === true)? '0':
-		Math.floor(duration/60);
-	 min_d = min_d <10 ? '0'+min_d:min_d;
+  // define minutes duration
+  let min_d = (isNaN(duration) === true)? '0':
+   Math.floor(duration/60);
+   min_d = min_d <10 ? '0'+min_d:min_d;
 
 
-	 function get_sec_d(x) {
-		if(Math.floor(x) >= 60){
+  function get_sec_d(x) {
+    if(Math.floor(x) >= 60){
 			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec_d = Math.floor(x) - (60*i);
-					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-				}
-			}
-		}else{
-		 	sec_d = (isNaN(duration) === true)? '0':
-		 	Math.floor(x);
-		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-		 }
-	} 
+      for (var i = 1; i<=60; i++){
+        if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+          sec_d = Math.floor(x) - (60*i);
+          sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+        }
+      }
+    } else {
+      sec_d = (isNaN(duration) === true)? '0':
+       Math.floor(x);
+       sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+    }
+  } 
 
-	// define seconds duration
-	get_sec_d(duration);
+  // define seconds duration
+  get_sec_d(duration);
 
-	// change duration DOM
-	durTime.innerHTML = min_d +':'+ sec_d;		
+  // change duration DOM
+  durTime.innerHTML = min_d +':'+ sec_d;		
 };
 
 // Event listeners
