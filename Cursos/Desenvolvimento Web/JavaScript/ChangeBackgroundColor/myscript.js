4fa969a10b17c05edf320bcd35c62ecae3bb4211
Gabriel Felippe <gabrielfelippe@outlook.com>
2023-04-25 13:39:28 -0300

    Alterado para Gerar Cor Aleatória

--- Cursos/Desenvolvimento Web/JavaScript/ChangeBackgroundColor/myscript.js
+++ Cursos/Desenvolvimento Web/JavaScript/ChangeBackgroundColor/myscript.js
@@ -1,5 +1,13 @@
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 document.getElementById("myButton").addEventListener("click", function() {
-  var colors = ["red", "green", "blue", "yellow"];
-  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  var randomColor = getRandomColor();
   document.body.style.backgroundColor = randomColor;
 });
