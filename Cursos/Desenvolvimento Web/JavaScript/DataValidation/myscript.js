efd7c45732ed05973151c611bc6c41ec2e96e40b
Gabriel Felippe <gabrielfelippe@outlook.com>
2023-04-29 00:32:44 -0300

    Exemplo Validação de Dados Melhorado

--- Cursos/Desenvolvimento Web/JavaScript/DataValidation/myscript.js
+++ Cursos/Desenvolvimento Web/JavaScript/DataValidation/myscript.js
@@ -1,7 +1,10 @@
 document.querySelector("form").addEventListener("submit", function(event) {
   var email = document.getElementById("email").value;
-  if (!email.includes("@")) {
+  var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!regex.test(email)) {
     event.preventDefault();
     alert("Please enter a valid email address.");
+  } else {
+    alert("Valid email!")
   }
 });
