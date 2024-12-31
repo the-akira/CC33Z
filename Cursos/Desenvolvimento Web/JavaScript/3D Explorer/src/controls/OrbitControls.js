4a930f280299cbd6c8e4d0d0276d34692899de14
Gabriel Felippe <gabrielfelippe@outlook.com>
2024-12-31 02:15:35 -0300

    3D Explorer - Melhorada Navegação da Grid

--- Cursos/Desenvolvimento Web/JavaScript/3D Explorer/src/controls/OrbitControls.js
+++ Cursos/Desenvolvimento Web/JavaScript/3D Explorer/src/controls/OrbitControls.js
@@ -1,5 +1,4 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import * as THREE from 'three';
 
 export function setupOrbitControls(camera, renderer) {
     const controls = new OrbitControls(camera, renderer.domElement);
